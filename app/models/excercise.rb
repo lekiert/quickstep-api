d53b636351a1b5035b0690524d3ad7dbdde431f1
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2016-11-05 13:43:14 +0100

    excercise model validation

--- app/models/excercise.rb
+++ app/models/excercise.rb
@@ -1,2 +1,9 @@
 class Excercise < ApplicationRecord
+  validates :code, presence: true, length: { minimum: 1, maximum: 15 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :command, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :status, presence: true,
+                     numericality: { only_integer: true,
+                                     greater_than_or_equal_to: -1,
+                                     less_than_or_equal_to: 1}
 end
