4a39ecc9bba491fdfc3e8eeefd38c1763aa672ea
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2016-11-07 19:22:15 +0100

    course resource test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def authenticated_header
+    token = Knock::AuthToken.new(payload: { sub: users(:one).id }).token
+
+    {
+      'Accept': 'application/vnd.api+json',
+      'Authorization': "Bearer #{token}"
+    }
+  end
+
+  def unauthenticated_header
+    {
+      'Accept': 'application/vnd.api+json',
+    }
+  end
+
+  test 'should respond success' do
+    get '/courses', headers: authenticated_header
+
+    assert_response :success
+  end
+
+  test 'should respond unauthorized' do
+    get '/courses', headers: unauthenticated_header
+
+    assert_response :unauthorized
+  end
 end
