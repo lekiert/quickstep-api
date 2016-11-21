9430ab7e564ef3137f705c4f8d48cc308e4b4284
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2016-11-21 22:14:34 +0100

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   match 'user_token' => 'user_token#create', via: :options
-
   jsonapi_resources :excercises
   jsonapi_resources :courses
+  jsonapi_resources :users
   jsonapi_resources :users do
-    jsonapi_resources :password_updates, via: :post
+    jsonapi_resources :password_updates
   end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
