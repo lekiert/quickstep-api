1a00d1050f3b6c18bb997be0a82e1db8efd76b3b
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2016-11-21 22:14:26 +0100

    added email to the user resource attributes

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -1,5 +1,6 @@
 class UserResource < JSONAPI::Resource
-  attributes :first_name, :last_name, :role
+  attributes :first_name, :last_name, :role, :email
 
   relationship :courses, to: :many
+  relationship :password_updates, to: :many
 end
