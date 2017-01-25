db5a1d07f906c6fb545e4fdd966516dd78ec3cb2
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2017-01-25 12:29:21 +0100

    storage fix

--- app/resources/storage_file_resource.rb
+++ app/resources/storage_file_resource.rb
@@ -1,5 +1,9 @@
 class StorageFileResource < JSONAPI::Resource
-  attributes :name, :description, :item
+  attributes :name, :description, :item, :mime
 
   relationship :excercises, to: :many
+
+  def mime
+    @model.item.instance.item_content_type
+  end
 end
