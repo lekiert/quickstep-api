85ec38e5a4f9ca8ebd482a45b44301dcf8b803c1
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2017-01-27 16:22:50 +0100

    max 2 answers per user

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationResourceController
+
+  def create
+    if !current_user.is_admin?
+      test_id = params[:data][:relationships][:test][:data].permit([:id]).to_h[:id]
+      if Answer.where(test_id: test_id, user_id: current_user.id).count >= 2
+        fail JSONAPI::Exceptions::SaveFailed.new()
+      end
+    end
+
+    super
+  rescue => e
+    handle_exceptions(e)
+    false
+  end
+
 end
