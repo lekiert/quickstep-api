efbf121a9a1b98c4bf3b8a423fa3d7518a4f4e28
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2017-01-28 22:19:44 +0100

    fix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,12 +2,12 @@ class AnswersController < ApplicationResourceController
   before_action :authenticate_user
 
   def create(*args)
-    if !current_user.is_admin?
-      test_id = params[:data][:relationships][:test][:data].permit([:id]).to_h[:id]
-      if Answer.where(test_id: test_id, user_id: current_user.id).count >= 2
-        fail JSONAPI::Exceptions::SaveFailed.new()
-      end
-    end
+    # if !current_user.is_admin?
+    #   test_id = params[:data][:relationships][:test][:data].permit([:id]).to_h[:id]
+    #   if Answer.where(test_id: test_id, user_id: current_user.id).count >= 2
+    #     fail JSONAPI::Exceptions::SaveFailed.new()
+    #   end
+    # end
 
     result = super(*args)
     DBLogger.info(current_user.id, 'USER_HAS_SUBMITTED_ANSWER', {
