5f810d8a9b9136c5d3745665c0fd0ee2b31e766c
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2017-03-24 00:51:43 +0100

    fix

--- app/resources/answer_resource.rb
+++ app/resources/answer_resource.rb
@@ -1,11 +1,22 @@
 class AnswerResource < JSONAPI::Resource
-  
+
   paginator :paged
 
   attributes :answers, :created_at, :results, :score, :test_id, :test_name
 
   relationship :user, to: :one
   relationship :test, to: :one
+
+  def self.records(options = {})
+    context = options[:context]
+    user = context[:current_user]
+    if !user.is_admin? && !user.is_supervisor?
+      context[:current_user].answers
+    else
+      super
+    end
+  end
+
 
   def created_at
     @model.created_at.strftime("%H:%S, %d.%m.%Y ")
